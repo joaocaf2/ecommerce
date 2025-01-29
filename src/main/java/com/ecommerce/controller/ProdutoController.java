8871e16c1f080d915b4b68eebc9e09b87a3834e4
João <Sem e-mail>
2025-01-28 22:20:05 -0300

    2 - Adicionando url para cadastrar produtos e teste unitário respectivo

---
+++ src/main/java/com/ecommerce/controller/ProdutoController.java
@@ -0,0 +1,14 @@
+package com.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @RequestMapping(value = "/cadastrar")
+    public String formulario() {
+        return "produtos/cadastrar-produto";
+    }
+}
---
+++ src/test/java/com/ecommerce/controller/ProdutoControllerTest.java
@@ -0,0 +1,28 @@
+package com.ecommerce.controller;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(ProdutoController.class)
+public class ProdutoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Página de formulário de cadastro deve ser chamada com sucesso")
+    public void deveChamarFormularioDeCadastroProdutoCorretamete() throws Exception {
+        mockMvc
+                .perform(get("/produtos/cadastrar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("produtos/cadastrar-produto"));
+    }
+}
