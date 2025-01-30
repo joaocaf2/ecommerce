ce87e119e2efa842aafb7dd15d858addc4858da4
João <Sem e-mail>
2025-01-30 11:34:08 -0300

    2- Incluindo a classe produto

--- src/main/java/com/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/ecommerce/controller/ProdutoController.java
@@ -1,14 +1,22 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Produto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-    @RequestMapping(value = "/cadastrar")
-    public String formulario() {
+    @RequestMapping(value = "/formulario")
+    public String formulario(Produto produto) {
         return "produtos/formulario-produto";
     }
+
+    @PostMapping(value = "/cadastrar")
+    public String cadastrar(Produto produto) {
+        return "redirect:/";
+    }
+
 }
--- src/test/java/com/ecommerce/controller/ProdutoControllerTest.java
+++ src/test/java/com/ecommerce/controller/ProdutoControllerTest.java
@@ -1,15 +1,14 @@
 package com.ecommerce.controller;
 
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(ProdutoController.class)
 public class ProdutoControllerTest {
@@ -17,12 +16,13 @@ public class ProdutoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("Página de formulário de cadastro deve ser chamada com sucesso")
-    public void deveChamarFormularioDeCadastroProdutoComSucesso() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = ("/produtos/formulario"))
+    @DisplayName("Deve retornar status ok ao acessar as urls do controller")
+    public void deveRetornarStatusOkAoAcessarAsUrlsDoController(String url) throws Exception {
         mockMvc
-                .perform(get("/produtos/cadastrar"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("produtos/formulario-produto"));
+                .perform(get(url))
+                .andExpect(status().isOk());
     }
+
 }
