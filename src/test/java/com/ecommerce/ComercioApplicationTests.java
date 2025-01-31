3c0af19368bb9add58f018ea5c48c1e0d790a18e
João <Sem e-mail>
2025-01-31 11:50:23 -0300

    2- Ajustando classe de teste para utilizar um banco de testes

--- src/test/java/com/ecommerce/ComercioApplicationTests.java
+++ src/test/java/com/ecommerce/ComercioApplicationTests.java
@@ -1,13 +1,24 @@
 package com.ecommerce;
 
+import com.ecommerce.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=password",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.show-sql=true"
+})
 class ComercioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
