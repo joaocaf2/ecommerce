0160c241528c84e4850601978d8db82979d2164e
João <Sem e-mail>
2025-01-31 20:24:52 -0300

    2- Criando teste unitário de gravar produto utilizando o formulário do controller

--- src/test/java/com/ecommerce/controller/ProdutoControllerTest.java
+++ src/test/java/com/ecommerce/controller/ProdutoControllerTest.java
@@ -2,6 +2,7 @@ package com.ecommerce.controller;
 
 import com.ecommerce.repository.ProdutoRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProdutoController.class)
@@ -31,6 +35,17 @@ public class ProdutoControllerTest {
         mockMvc
                 .perform(get(url))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Deve cadastrar o produto com sucesso ao submeter o formulário")
+    public void deveCadastrarOProdutoComSucessoAoSubmeterOFormulario() throws Exception {
+        mockMvc.perform(post("/produtos/cadastrar")
+                        .param("nome", "Produto Teste")
+                        .param("descricao", "Descrição do produto")
+                        .param("preco", "100.00"))
+                .andExpect(redirectedUrl("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
     }
 
 }
