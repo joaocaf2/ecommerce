c8e4ffa21c2683274b252a6db84c1f182656809d
João <Sem e-mail>
2025-01-31 12:01:53 -0300

    2- Criando propriedades de teste e utilizando na classe de teste

--- src/test/java/com/ecommerce/ComercioApplicationTests.java
+++ src/test/java/com/ecommerce/ComercioApplicationTests.java
@@ -1,20 +1,11 @@
 package com.ecommerce;
 
-import com.ecommerce.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.datasource.driverClassName=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=password",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.show-sql=true"
-})
+@ActiveProfiles("test")
 class ComercioApplicationTests {
 
     @Test
